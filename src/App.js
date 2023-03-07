8889b0b5a787b63e19aff6c9094f75ff4175b52f
JasonMartin85 <jason.martin.85@gmail.com>
2023-03-07 12:17:23 -0500

    Added additional test data for merge testing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Welcome to Project 2!</h1>
         {/* Test comment please ignore! */}
+        {/* This is a test branch */}
       </header>
     </div>
   );
