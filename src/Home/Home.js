fdbf2bce905dfa9fca8063a384b14e1a7cb806e1
JasonMartin85 <jason.martin.85@gmail.com>
2023-03-08 17:18:34 -0500

    Added links and search bar functionality to navbar component, updated home component random image to link to card details page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
+import {useNavigate} from 'react-router-dom'
 
 const Home = () => {
   const [currCard, setCurrCard] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`https://api.scryfall.com/cards/random`)
       .then((res) => res.json())
       .then((data) => {
-        setCurrCard(data.image_uris);
+        setCurrCard(data);
       });
   }, []);
 
@@ -16,16 +18,21 @@ const Home = () => {
   }
 
   return (
+    <>
+    {Object.keys(currCard).length !== 0 ?
     <div>
     {/*    {JSON.stringify(currCard.border_crop)}
        {console.log(JSON.stringify(currCard.border_crop))} */}
-      <img src={currCard.normal} />
+      <img src={currCard.image_uris.normal} onClick={()=>navigate(`/DetailView/${currCard.id}`)} />
       {/* {currCard.image_uris.large ? (
         <img src={currCard.image_uris.large} />
       ) : (
         <h1>Loading...</h1>
       )} */}
     </div>
+    :
+    <div>Loading...</div>}
+    </>
   );
 };
 
--- src/common/NavBar.js
+++ src/common/NavBar.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
+import {Link} from "react-router-dom"
+import './NavBar.css'
 
 const NavBar = () => {
-  return <></>;
-};
+  const [searchTerm,setSearchTerm] = useState('')
+
+  return (
+    <nav className="navBar">
+      <Link className ="link" to="/">Home</Link>
+      <Link className ="link" to="/MyDeck">My Deck</Link>
+      <Link className ="link" to="/FeaturedDecks">Featured Decks</Link>
+      {/* <Link to="/Tutorial">Tutorial</Link> */}
+      <input className="searchBar" type="search" placeholder="Search..." onChange={e=>setSearchTerm(e.target.value)}/>
+      <Link className="searchBtn" to={`/Search/${searchTerm}`}><button>Search!</button></Link>
+    </nav>
+  )};
 
 export default NavBar;
