68f1e8634ff7986426ba978ede2e1b4ad69e94dd
ddbonilla <ddbonillav@gmail.com>
2023-03-09 07:19:43 -0800

    ready to be pushed

--- src/Tutorial/Tutorial.js
+++ src/Tutorial/Tutorial.js
@@ -3,7 +3,7 @@ import "./Tutorial.css";
 const Tutorial = () => {
     return (
         <div className="wrapper relative flex w-full flex-wrap items-center justify-center shadow-lg">
-            <div className="text-white text-lg">In construction</div>
+            <div className="text-white text-lg">Under Construction</div>
         </div>
     );
 }
