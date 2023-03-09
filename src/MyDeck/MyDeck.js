df8f3babc391635e31df4490f95fa80b982c7aee
ddbonilla <ddbonillav@gmail.com>
2023-03-09 07:56:23 -0800

    branch creation

--- src/MyDeck/MyDeck.js
+++ src/MyDeck/MyDeck.js
@@ -1,6 +1,11 @@
 import React from "react";
+import "./MyDeck.css";
 
 const MyDeck = () => {
-  return <></>;
+  return (
+    <div className="w-full flex justify-center items-center mt-10">
+      Test
+    </div>
+  )
 };
 export default MyDeck;
