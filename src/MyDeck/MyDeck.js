35a09134c8744ac65991bdebee2f2fc9825b9026
ddbonilla <ddbonillav@gmail.com>
2023-03-09 12:59:04 -0800

    premerge

--- src/MyDeck/MyDeck.js
+++ src/MyDeck/MyDeck.js
@@ -1,11 +1,236 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./MyDeck.css";
 
 const MyDeck = () => {
+  const [decks, setDecks] = useState([]);
+  const [image, setImage] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("http://localhost:8000/DeckArray")
+      .then((res) => res.json())
+      .then((data) => {
+        setDecks(data);
+      });
+  }, []);
+
+  const countArr = [];
+
+  let artifactCards = 0;
+  let creatureCards = 0;
+  let enchantmentCards = 0;
+  let instantCards = 0;
+  let landCards = 0;
+  let planeswalkerCards = 0;
+  let sorceryCards = 0;
+
+  for (let i = 0; i < decks.length; i++) {
+    if (decks[i].cardObj.type_line === "Artifact") {
+      artifactCards++;
+    }
+
+    if (decks[i].cardObj.type_line === "Creature") {
+      creatureCards++;
+    }
+    if (decks[i].cardObj.type_line === "Enchantment") {
+      enchantmentCards++;
+    }
+    if (decks[i].cardObj.type_line === "Instant") {
+      instantCards++;
+    }
+    if (decks[i].cardObj.type_line === "Land") {
+      landCards++;
+    }
+    if (decks[i].cardObj.type_line === "Planeswalker") {
+      planeswalkerCards++;
+    }
+    if (decks[i].cardObj.type_line === "Sorcery") {
+      sorceryCards++;
+    }
+  }
+
+  countArr.push(
+    artifactCards,
+    creatureCards,
+    enchantmentCards,
+    instantCards,
+    landCards,
+    planeswalkerCards,
+    sorceryCards
+  );
+  console.log(countArr);
+
   return (
-    <div className="w-full flex justify-center items-center mt-10">
-      Test
+    <div className="mt-10 mx-24">
+      <h1 className="text-2xl font-bold p-3"> Decklist</h1>
+
+      <div className="relative grid grid-rows-2 grid-cols-2 mt-1">
+        <div className="deck-list text-sm">
+          <div className="grid grid-cols-2">
+            <div className="col-1 p-5">
+              <div className="font-bold italic">
+                Artifacts ({countArr[0] === 0 ? 0 : countArr[0]})
+              </div>
+              <ul className="list-inside">
+                {decks.map((card) =>
+                  card.cardObj.type_line === "Artifact" ? (
+                    <li key={card.cardId}>
+                      {card.count}{" "}
+                      <a
+                        href="#1"
+                        target="test"
+                        onMouseOver={(e) => setImage(e.currentTarget.target)}
+                        onClick={() => navigate(`/DetailView/${card.cardId}`)}
+                      >
+                        {card.cardObj.name}
+                      </a>
+                    </li>
+                  ) : (
+                    ""
+                  )
+                )}
+              </ul>
+              <div className="font-bold italic mt-5">
+                Creature ({countArr[1]})
+              </div>
+              <ul className="list-inside">
+                {decks.map((card) =>
+                  card.cardObj.type_line === "Creature" ? (
+                    <li key={card.cardId}>
+                      {card.count}{" "}
+                      <a
+                        href="#2"
+                        target="test2"
+                        onMouseOver={(e) => setImage(e.currentTarget.target)}
+                        onClick={() => navigate(`/DetailView/${card.cardId}`)}
+                      >
+                        {card.cardObj.name}
+                      </a>
+                    </li>
+                  ) : (
+                    ""
+                  )
+                )}
+              </ul>
+              <div className="font-bold italic mt-5">
+                Enchantment ({countArr[2]})
+              </div>
+              <ul className="list-inside">
+                {decks.map((card) =>
+                  card.cardObj.type_line === "Enchantment" ? (
+                    <li key={card.cardId}>
+                      {card.count}{" "}
+                      <a
+                        href="#3"
+                        target="test3"
+                        onMouseOver={(e) => setImage(e.currentTarget.target)}
+                        onClick={() => navigate(`/DetailView/${card.cardId}`)}
+                      >
+                        {card.cardObj.name}
+                      </a>
+                    </li>
+                  ) : (
+                    ""
+                  )
+                )}
+              </ul>
+              <div className="font-bold italic mt-5">
+                Instant ({countArr[3]})
+              </div>
+              <ul className="list-inside">
+                {decks.map((card) =>
+                  card.cardObj.type_line === "Instant" ? (
+                    <li key={card.cardId}>
+                      {card.count}{" "}
+                      <a
+                        href="#4"
+                        target="test4"
+                        onMouseOver={(e) => setImage(e.currentTarget.target)}
+                        onClick={() => navigate(`/DetailView/${card.cardId}`)}
+                      >
+                        {card.cardObj.name}
+                      </a>
+                    </li>
+                  ) : (
+                    ""
+                  )
+                )}
+              </ul>
+            </div>
+            <div className="col-1 p-5">
+              <div className="font-bold italic">Land ({countArr[4]})</div>
+              <ul className="list-inside">
+                {decks.map((card) =>
+                  card.cardObj.type_line === "Land" ? (
+                    <li key={card.cardId}>
+                      {card.count}{" "}
+                      <a
+                        href="#5"
+                        target="test5"
+                        onMouseOver={(e) => setImage(e.currentTarget.target)}
+                        onClick={() => navigate(`/DetailView/${card.cardId}`)}
+                      >
+                        {card.cardObj.name}
+                      </a>
+                    </li>
+                  ) : (
+                    ""
+                  )
+                )}
+              </ul>
+              <div className="font-bold italic mt-5">
+                Planeswalker ({countArr[5]})
+              </div>
+              <ul className="list-inside">
+                {decks.map((card) =>
+                  card.cardObj.type_line === "Planeswalker" ? (
+                    <li key={card.cardId}>
+                      {card.count}{" "}
+                      <a
+                        href="#6"
+                        target="test6"
+                        onMouseOver={(e) => setImage(e.currentTarget.target)}
+                        onClick={() => navigate(`/DetailView/${card.cardId}`)}
+                      >
+                        {card.cardObj.name}
+                      </a>
+                    </li>
+                  ) : (
+                    ""
+                  )
+                )}
+              </ul>
+              <div className="font-bold italic mt-5">
+                Sorcery ({countArr[6]})
+              </div>
+              <ul className="list-inside">
+                {decks.map((card) =>
+                  card.cardObj.type_line === "Sorcery" ? (
+                    <li key={card.cardId}>
+                      {card.count}{" "}
+                      <a
+                        href="#7"
+                        target="test7"
+                        onMouseOver={(e) => setImage(e.currentTarget.target)}
+                        onClick={() => navigate(`/DetailView/${card.cardId}`)}
+                      >
+                        {card.cardObj.name}
+                      </a>
+                    </li>
+                  ) : (
+                    ""
+                  )
+                )}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="deck-img">{image}</div>
+      </div>
     </div>
-  )
+  );
 };
+
 export default MyDeck;
--- src/common/NavBar.js
+++ src/common/NavBar.js
@@ -6,19 +6,19 @@ const NavBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <div className="nav-wrapper relative flex w-full flex-wrap items-center justify-center shadow-lg">
+    <div className="nav-wrapper relative w-full h-full overflow-auto flex flex-wrap items-center justify-center shadow-lg">
       <div className="brand-ribbon py-1">
         <Link className="link p-3 text-5xl" to="/">
-          <i class="brand-logo ss ss-parl3 p-3">
+          <i className="brand-logo ss ss-parl3 p-3">
             <span>rcanum</span>
           </i>
         </Link>
       </div>
-      <nav className="nav-bar relative flex w-full items-center justify-around shadow-lg px-3">
+      <nav className="nav-bar relative w-full flex items-center justify-around shadow-lg px-3">
           <div className="links relative inline-flex items-center">
             <Link className="relative inline-flex items-center p-3" to="/MyDeck">
               <i className="deck-icon ss ss-s00 p-1"/><span> My Deck</span>
-              {/* <div class="absolute inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-red-500 rounded-full top-2 -right-0">4</div> */}
+              <div className="absolute inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-red-500 rounded-full top-2 -right-0">4</div>
             </Link>
             |
             <Link className="link p-3" to="/FeaturedDecks">
@@ -29,7 +29,7 @@ const NavBar = () => {
               <i className="deck-icon ss ss-atq p-1"/><span> Tutorial</span>
             </Link>
           </div>
-          <div className="search-bundle">
+          <div className="search-bundle flex-shrink-0">
             <input
               className="search-input text-black italic"
               type="search"
