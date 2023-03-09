8ab4550e882867c5e54ef5de75fc0b6b85f35091
Jacob S. Steward <jacobtheeldest@gmail.com>
2023-03-09 10:59:05 -0500

    Add title to Featured Decks

--- src/FeaturedDecks/FeaturedDecks.js
+++ src/FeaturedDecks/FeaturedDecks.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Decklist from "./Decklist";
 
 
 const FeaturedDecks = () => {
   
+  useEffect(()=> {
+    document.title = 'Arcanum: Featured Decks'
+  },[])
+
   return (
     <>
       These are some Featured Decks
